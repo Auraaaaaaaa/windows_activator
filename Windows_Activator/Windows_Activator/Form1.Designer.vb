9f7fbc98454e312efe316dd92dcd0255dc5e2947
Chazzy123867 <56513089+Chazzy123867@users.noreply.github.com>
2020-11-17 16:17:47 +0000

    Add files via upload

---
+++ Windows_Activator/Windows_Activator/Form1.Designer.vb
@@ -0,0 +1,151 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.RadioButton4)
+        Me.GroupBox1.Controls.Add(Me.RadioButton3)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Location = New System.Drawing.Point(10, 10)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(2)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(2)
+        Me.GroupBox1.Size = New System.Drawing.Size(569, 289)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Select your Windows 10 Version THAT IS CURRENTLY INSTALLED"
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(311, 82)
+        Me.RadioButton4.Margin = New System.Windows.Forms.Padding(2)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(200, 24)
+        Me.RadioButton4.TabIndex = 6
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "Home (country specific)"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(115, 82)
+        Me.RadioButton3.Margin = New System.Windows.Forms.Padding(2)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(166, 24)
+        Me.RadioButton3.TabIndex = 5
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "Home (single lang)"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.14286!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.Maroon
+        Me.Button1.Location = New System.Drawing.Point(5, 196)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(559, 88)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Hacktivate!"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(14, 82)
+        Me.RadioButton2.Margin = New System.Windows.Forms.Padding(2)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(77, 24)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Home"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(14, 38)
+        Me.RadioButton1.Margin = New System.Windows.Forms.Padding(2)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(121, 24)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Professional"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Red
+        Me.Label1.Location = New System.Drawing.Point(14, 151)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(289, 26)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Does not support windows N"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(588, 309)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Windows 10 Activator by Imarshmellow10"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Windows_Activator/Windows_Activator/Form1.vb
@@ -0,0 +1,60 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("Windows will now activate, some messaage boxes will appear after this program closes, that is just windows telling you, there should be 3-4")
+        My.Computer.Registry.SetValue("HKEY_CURRENT_USER\Control Panel\Desktop", "PaintDesktopVersion", 4, Microsoft.Win32.RegistryValueKind.DWord)
+        Shell("cmd.exe /c gpupdate /force")
+        My.Computer.Registry.SetValue("HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\svsvc", "Start", 4, Microsoft.Win32.RegistryValueKind.DWord)
+        My.Computer.Registry.LocalMachine.CreateSubKey("SYSTEM\CurrentControlSet\Services\svsvc\KMS")
+        My.Computer.Registry.LocalMachine.OpenSubKey("SYSTEM\CurrentControlSet\Services\svsvc\KMS", True).SetValue("(Default)", "kms_4")
+        Shell("cmd.exe /c gpupdate /force")
+        'System.Threading.Thread.CurrentThread.Sleep(5000)
+        If RadioButton1.Checked = True Then
+            Shell("cmd.exe /c slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX")
+            ' System.Threading.Thread.CurrentThread.Sleep(10000)
+            Shell("cmd.exe /c slmgr /skms kms8.msguides.com")
+            ' System.Threading.Thread.CurrentThread.Sleep(10000)
+            Shell("cmd.exe /c slmgr /ato")
+            My.Computer.Registry.SetValue("HKEY_CURRENT_USER\Control Panel\Desktop", "PaintDesktopVersion", 0, Microsoft.Win32.RegistryValueKind.DWord)
+            Shell("cmd.exe /c gpupdate /force")
+            ' System.Threading.Thread.CurrentThread.Sleep(5000)
+            'MsgBox("Done! Please refresh your desktop, to get rid of the version message")
+        ElseIf RadioButton2.Checked = True Then
+            Shell("cmd.exe /c slmgr /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99")
+            'System.Threading.Thread.CurrentThread.Sleep(10000)
+            Shell("cmd.exe /c slmgr /skms kms8.msguides.com")
+            'System.Threading.Thread.CurrentThread.Sleep(10000)
+            Shell("cmd.exe /c slmgr /ato")
+            My.Computer.Registry.SetValue("HKEY_CURRENT_USER\Control Panel\Desktop", "PaintDesktopVersion", 0, Microsoft.Win32.RegistryValueKind.DWord)
+            Shell("cmd.exe /c gpupdate /force")
+            'System.Threading.Thread.CurrentThread.Sleep(5000)
+            'MsgBox("Done! Please refresh your desktop, to get rid of the version message")
+        ElseIf RadioButton3.Checked = True Then
+            Shell("cmd.exe /c slmgr /ipk 7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH")
+            'System.Threading.Thread.CurrentThread.Sleep(10000)
+            Shell("cmd.exe /c slmgr /skms kms8.msguides.com")
+            ' System.Threading.Thread.CurrentThread.Sleep(10000)
+            Shell("cmd.exe /c slmgr /ato")
+            My.Computer.Registry.SetValue("HKEY_CURRENT_USER\Control Panel\Desktop", "PaintDesktopVersion", 0, Microsoft.Win32.RegistryValueKind.DWord)
+            Shell("cmd.exe /c gpupdate /force")
+            'System.Threading.Thread.CurrentThread.Sleep(5000)
+            'MsgBox("Done! Please refresh your desktop, to get rid of the version message")
+        ElseIf RadioButton4.Checked = True Then
+            Shell("cmd.exe /c slmgr /ipk PVMJN-6DFY6-9CCP6-7BKTT-D3WVR")
+            'System.Threading.Thread.CurrentThread.Sleep(10000)
+            Shell("cmd.exe /c slmgr /skms kms8.msguides.com")
+            '  System.Threading.Thread.CurrentThread.Sleep(10000)
+            Shell("cmd.exe /c slmgr /ato")
+            My.Computer.Registry.SetValue("HKEY_CURRENT_USER\Control Panel\Desktop", "PaintDesktopVersion", 0, Microsoft.Win32.RegistryValueKind.DWord)
+            Shell("cmd.exe /c gpupdate /force")
+            '  System.Threading.Thread.CurrentThread.Sleep(5000)
+            'MsgBox("Done! Please refresh your desktop, to get rid of the version message")
+        End If
+        MsgBox("Should be done, check your windows settings and see if you can personalize your computer.")
+        'MsgBox("Thank you for using my windows activator")
+        Application.Exit()
+    End Sub
+    'To deactivate windows, type slmgr /upk into an admin command prompt
+
+
+
+End Class
